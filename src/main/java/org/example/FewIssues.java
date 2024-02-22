1c29d596db702b8cd1016f44d35fdf2f627d39a2
Eray Felek <eray.felek@sonarsource.com>
2024-02-22 10:48:49 +0100

    a

---
+++ src/main/java/org/example/FewIssues.java
@@ -0,0 +1,14 @@
+package org.example;
+
+public class FewIssues {
+  public static void main(String[] args) {
+    System.out.println("Hello world!");
+    System.out.println("Hello world!");
+    System.out.println("Hello world!");
+    System.out.println("Hello world!");
+    System.out.println("Hello world!");//eray
+    int a;
+    System.out.println("Hello world!");
+    System.out.println("Hello world!");
+  }
+}
