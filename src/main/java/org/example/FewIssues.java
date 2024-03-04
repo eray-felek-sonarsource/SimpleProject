91962d9ddc577d85e25cf7bfa7e427aab4518e82
Eray Felek <eray.felek@sonarsource.com>
2024-03-04 09:47:31 +0100

    a

--- src/main/java/org/example/FewIssues.java
+++ src/main/java/org/example/FewIssues.java
@@ -1,5 +1,11 @@
 package org.example;
 
+import java.net.InetAddress;
+import java.security.SecureRandom;
+import java.sql.DriverManager;
+
+import static com.sun.org.apache.xml.internal.serialize.OutputFormat.Defaults.Encoding;
+
 public class FewIssues {
   public static void main(String[] args) {
     System.out.println("Hello world!");
@@ -13,6 +19,9 @@ public class FewIssues {
     System.out.println("Hello world!");
     System.out.println("Hello world!");
     System.out.println("Hello world!");
+    System.out.println("Hello world!");//eray
+    int a;
     System.out.println("Hello world!");
   }
+
 }
