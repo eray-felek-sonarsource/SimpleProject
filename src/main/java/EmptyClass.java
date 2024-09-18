49b2f8f569143ffd845c85681e8b146d734d08df
Eray Felek <eray.felek@sonarsource.com>
2024-09-18 13:49:09 +0200

    ds

--- src/main/java/EmptyClass.java
+++ src/main/java/EmptyClass.java
@@ -1,5 +1,24 @@
 public class EmptyClass {
     //TODO
+    public boolean hasNextOccurence() {
+        // relies on the assumption that a TreeNodes will always be the last row in the table view of the tree
+        var isOldTree = tree.getSelectionPath() == null;
+        var path = isOldTree ? oldTree.getSelectionPath() : tree.getSelectionPath();
+        if (path == null) {
+            return false;
+        }
+
+        return isOldTree ? fetchNextOccurenceInfo(path, oldTree) : fetchNextOccurenceInfo(path, tree);
+    }
+
+    private static boolean fetchNextOccurenceInfo(TreePath path, Tree tree) {
+        var node = (DefaultMutableTreeNode) path.getLastPathComponent();
+        if (node instanceof IssueNode) {
+            return tree.getRowCount() != tree.getRowForPath(path) + 1;
+        } else {
+            return node.getChildCount() > 0;
+        }
+    }
     //TODO
     //TODO
     //TODO
